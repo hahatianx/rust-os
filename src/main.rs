5ee573322882231769c343d3fb2076f9bfd58a6a
shuhans <shuhan.x.shen@gmail.com>
2024-03-09 10:57:43 -0800

    basic env setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
-#![no_std]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
 
-fn main() { }
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(_info);
+}
